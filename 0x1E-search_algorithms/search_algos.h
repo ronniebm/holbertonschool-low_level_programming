965db22e18fc31281b298fb1f894465b374f92bf
ronniebm <ronnie.coding@gmail.com>
2020-08-27 18:14:05 -0500

    adding prototype to library file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,5 +5,6 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int b_search_rec(int *array, int left, int right, int val);
 
 #endif /* _CALC_H_ */
